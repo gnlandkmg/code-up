79ee5eca6748cc925e3daec79d3ec1bceecb6343
gnlandkmg <gnlandkmg@dgsw.hs.kr>
2025-03-17 17:41:38 +0900

    initial commit

---
+++ 0305.py
@@ -0,0 +1,4 @@
+name = "jung do hyun"
+age = 30
+hobby = "talking"
+print(f"My name is {name}. I am {age} years old, and I like {hobby}.")
---
+++ 0311.py
@@ -0,0 +1,7 @@
+n = "a"
+n = str(input())  
+while n != "q" :
+    print(n)
+    n = str(input())
+if n == "q" :
+    print(n)
