6be027b04585764456453b997f5c5334e5356cc2
gnlandkmg <gnlandkmg@dgsw.hs.kr>
2025-03-19 11:59:44 +0900

    25.03.19-practice

---
+++ 18.py
@@ -0,0 +1,8 @@
+a = int(input())
+if a >=80:
+    print("A")
+elif a >= 70:
+    print("B")
+else:
+    print("C")
+print("END")
---
+++ 20.py
@@ -0,0 +1,6 @@
+id = input("아이디를 입력하세요.")
+pw = input("비밀번호를 입력하세요.")
+if id == "dgsw" and pw == "high":
+    print("로그인에 성공했습니다.")
+else:
+    print("로그인에 실패했습니다.")
