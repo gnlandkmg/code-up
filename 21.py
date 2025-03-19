9b81852efab7a6b7c4572bc792f8942c9dfedde8
gnlandkmg <gnlandkmg@dgsw.hs.kr>
2025-03-19 12:00:07 +0900

    25.03.19-practice

---
+++ 21.py
@@ -0,0 +1,8 @@
+cm = int(input("키(cm):"))
+kg= int(input("체중(kg):"))
+
+print("#"*10)
+if 130 <=cm <190 and 25 <= kg < 100:
+    print("이용 가능")
+else:
+        print("이용 불가능")
---
+++ 22.py
@@ -0,0 +1,5 @@
+str = "Hello, my friends!"
+if "a" in str:
+    print("있습니다.")
+else:
+    print("없습니다.")
